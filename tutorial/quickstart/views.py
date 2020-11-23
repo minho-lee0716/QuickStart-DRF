d5cca7ab69e82d4e929d7e625bf427ac4543c624
Min-Ho Lee <minho.lee0716@gmail.com>
2020-11-24 00:43:09 +0900

    Add: views app & views.py test2

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -20,20 +20,3 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-class ListUsers(APIView):
-    """
-    View to list all users in the system.
-
-    * Requires token authentication.
-    * Only admin users are able to access this view.
-    """
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
-
-    def get(self, request, format=None):
-        """
-        Return a list of all users.
-        """
-        usernames = [user.username for user in User.objects.all()]
-        return Response(usernames)
